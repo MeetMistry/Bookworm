f46f2ab19492849d718aa1373d3f9b5f7b7c19b2
Meet Mistry <meetmistry138@gmail.com>
2024-10-31 08:44:54 +0530

    Initial commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Meet Mistry on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
